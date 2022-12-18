86a99e03cc24e92e01650400c24bf6a9ca9ef546
Enzo Mourany <enzo.mourany@outlook.com>
2022-12-19 00:26:33 +0100

    feat: add initial text on editor

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,7 +1,29 @@
 import React, { useCallback, useEffect } from 'react';
+import useCodeMirror from "../utils/codeMirror";
 
-const Editor: React.FC = () => {
-  return <div>Editor</div>;
+interface Props {
+  initialDoc: string,
+  onChange: (doc: string) => void
+}
+
+const Editor: React.FC<Props> = (props) => {
+  const { onChange } = props;
+  const handleChange = useCallback(
+    (state: { doc: { toString: () => string; }; }) => onChange(state.doc.toString()),
+    [onChange]
+  )
+  const [refContainer, editorView] = useCodeMirror<HTMLDivElement>({
+    initialDoc: 'Welcome',
+    onChange: handleChange
+  });
+
+  useEffect(() => {
+    if (editorView) {
+
+    }
+  }, [editorView]);
+
+  return <div ref={refContainer}>Editor</div>;
 };
 
 export default Editor;
