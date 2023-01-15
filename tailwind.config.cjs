8e41e9881b37ba5097728ceb19489e9752c4bbf1
Enzo Mourany <enzo.mourany@outlook.com>
2023-01-15 19:04:21 +0100

    feat: add colors config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      typography: {
+          css: {
+            h2: {
+              color: colors.red['600'],
+            },
+          },
+      },
+    },
   },
   plugins: [require("daisyui"), require('@tailwindcss/typography'),],
 }
